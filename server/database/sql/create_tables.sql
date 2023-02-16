abb3db87fed32252db85df9d5d31d1e32c2460ae
spope <spope851@gmail.com>
2023-02-15 19:13:20 -0700

    add users table to create tables script

--- server/database/sql/create_tables.sql
+++ server/database/sql/create_tables.sql
@@ -1,3 +1,11 @@
+create table if not exists users (
+    id serial primary key,
+    full_name varchar (255),
+    email varchar (255) not null,
+    username varchar (255) not null,
+    password varchar (255) not null
+);
+
 create table if not exists tracker (
     id serial primary key
 );
