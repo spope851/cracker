3287e6bf69ea2bbc804cb0391a99a22696a1ba76
spencer pope <34104632+spope851@users.noreply.github.com>
2023-02-27 21:00:27 -0500

    Feature/3 register UI (#4)
    * add users table to create tables script
    
    * register resolver that inserts new user into the db
    
    * add support for redis to server
    
    * add created/updated at fields. add unique constraint to username and email fields
    
    * return user from register mutation
    
    * registration error handling for duplicate username/password
    
    * add role pk to user schema. add role lookup table and two roles. rename "users" table to "user". rename "full_name" field to "name"
    
    * remove name field from user schema. refactor register mutation
    
    * setup next auth session service. add me query. add userId to redis from next auth callback. add userId to apollo context from redis. query db for userId in me query and return user. add role field to user class. remove getUser query
    
    * refactor me query. add user to next auth token. leave comment in ApolloServer constructor in case we ever want to store something in context
    
    * remove export
    
    * user avatar component. signup/signin components. admin page with timeclock component
    
    * remove console log. remove unneeded middleware that was causing build error
    
    * signin loading state
    
    * loading state ui
    
    * refactor signin loading

---
+++ components/avatarMenu.tsx
@@ -0,0 +1,129 @@
+import { graphql } from "@/generated"
+import { useQuery } from "@apollo/client"
+import { Logout } from "@mui/icons-material"
+// import { Settings } from "@mui/icons-material"
+import {
+  IconButton,
+  Avatar,
+  Menu,
+  MenuItem,
+  Divider,
+  ListItemIcon,
+  Box,
+  Typography,
+} from "@mui/material"
+import { useRouter } from "next/router"
+import React, { useState } from "react"
+import AdminPanelSettingsIcon from "@mui/icons-material/AdminPanelSettings"
+import { signOut } from "next-auth/react"
+
+interface UserAvatarProps {
+  id: string
+  token: string
+}
+
+const AvatarMenu: React.FC<UserAvatarProps> = ({ id, token }) => {
+  const router = useRouter()
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null)
+  const open = Boolean(anchorEl)
+  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget)
+  }
+  const handleClose = () => {
+    setAnchorEl(null)
+  }
+
+  const { loading, data } = useQuery(
+    graphql(`
+      query Me($user: UserAuthInput!) {
+        me(user: $user) {
+          error
+          user {
+            email
+            id
+            role
+            username
+          }
+        }
+      }
+    `),
+    {
+      variables: {
+        user: { id, token },
+      },
+    }
+  )
+
+  return (
+    <>
+      <IconButton
+        onClick={handleClick}
+        size="small"
+        aria-controls={open ? "account-menu" : undefined}
+        aria-haspopup="true"
+        aria-expanded={open ? "true" : undefined}
+        sx={{ p: 0 }}
+      >
+        <Avatar />
+      </IconButton>
+      <Menu
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        onClick={handleClose}
+        PaperProps={{
+          elevation: 0,
+          sx: {
+            overflow: "visible",
+            filter: "drop-shadow(0px 2px 8px rgba(0,0,0,0.32))",
+            mt: 1.5,
+            "&:before": {
+              content: '""',
+              display: "block",
+              position: "absolute",
+              top: 0,
+              right: 7,
+              width: 10,
+              height: 10,
+              bgcolor: "background.paper",
+              transform: "translateY(-50%) rotate(45deg)",
+              zIndex: 0,
+            },
+          },
+        }}
+        transformOrigin={{ horizontal: "right", vertical: "top" }}
+        anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
+      >
+        <Box display="flex" p={1} justifyContent="center" alignItems="center">
+          <Avatar />
+          <Typography ml={1}>
+            {loading ? "...loading" : data?.me.user?.username}
+          </Typography>
+        </Box>
+        <Divider />
+        {data?.me.user?.role === 2 && (
+          <MenuItem onClick={() => router.push("/admin")}>
+            <ListItemIcon>
+              <AdminPanelSettingsIcon fontSize="small" />
+            </ListItemIcon>
+            Admin
+          </MenuItem>
+        )}
+        {/* <MenuItem onClick={handleClose}>
+          <ListItemIcon>
+            <Settings fontSize="small" />
+          </ListItemIcon>
+          Settings
+        </MenuItem> */}
+        <MenuItem onClick={() => signOut()}>
+          <ListItemIcon>
+            <Logout fontSize="small" />
+          </ListItemIcon>
+          Logout
+        </MenuItem>
+      </Menu>
+    </>
+  )
+}
+
+export default AvatarMenu
