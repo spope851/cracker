5ab8cc97c9f69caec642b4ac8ab46f25d6dcd4ec
spencer pope <34104632+spope851@users.noreply.github.com>
2023-03-01 18:00:16 -0500

    Draft: tracker table schema (#5)
    * add users table to create tables script
    
    * register resolver that inserts new user into the db
    
    * add support for redis to server
    
    * add created/updated at fields. add unique constraint to username and email fields
    
    * return user from register mutation
    
    * registration error handling for duplicate username/password
    
    * add role pk to user schema. add role lookup table and two roles. rename "users" table to "user". rename "full_name" field to "name"
    
    * remove name field from user schema. refactor register mutation
    
    * add users table to create tables script (#2)
    
    * add users table to create tables script
    
    * add created/updated at fields. add unique constraint to username and email fields
    
    * add role pk to user schema. add role lookup table and two roles. rename "users" table to "user". rename "full_name" field to "name"
    
    * setup next auth session service. add me query. add userId to redis from next auth callback. add userId to apollo context from redis. query db for userId in me query and return user. add role field to user class. remove getUser query
    
    * refactor me query. add user to next auth token. leave comment in ApolloServer constructor in case we ever want to store something in context
    
    * remove export
    
    * Feature/2 register request (#3)
    
    * add users table to create tables script
    
    * register resolver that inserts new user into the db
    
    * add support for redis to server
    
    * add created/updated at fields. add unique constraint to username and email fields
    
    * return user from register mutation
    
    * registration error handling for duplicate username/password
    
    * add role pk to user schema. add role lookup table and two roles. rename "users" table to "user". rename "full_name" field to "name"
    
    * remove name field from user schema. refactor register mutation
    
    * setup next auth session service. add me query. add userId to redis from next auth callback. add userId to apollo context from redis. query db for userId in me query and return user. add role field to user class. remove getUser query
    
    * refactor me query. add user to next auth token. leave comment in ApolloServer constructor in case we ever want to store something in context
    
    * remove export
    
    * user avatar component. signup/signin components. admin page with timeclock component
    
    * remove console log. remove unneeded middleware that was causing build error
    
    * signin loading state
    
    * loading state ui
    
    * refactor signin loading
    
    * tracker table schema. commented out unique index. left some comments with helpful queries too. new:db script

--- server/database/sql/create_tables.sql
+++ server/database/sql/create_tables.sql
@@ -1,14 +1,14 @@
 create table if not exists role_lookup (
     id serial primary key,
-    name varchar (255) not null unique,
+    name varchar (10) not null unique,
     description varchar (255) not null
 );
 
 create table if not exists "user" (
     id serial primary key,
-    username varchar (255) not null unique,
-    password varchar (255) not null,
-    email varchar (255) not null unique,
+    username varchar (50) not null unique,
+    password varchar (100) not null,
+    email varchar (100) not null unique,
     created_at timestamp default now(),
     updated_at timestamp default now(),
     role integer default 1,
@@ -16,5 +16,43 @@ create table if not exists "user" (
 );
 
 create table if not exists tracker (
-    id serial primary key
+    id serial primary key,
+    overview varchar (512) not null,
+    number_creative_hours numeric (3,1) not null,
+    rating smallint not null,
+    created_at timestamp default now(),
+    "user" integer not null,
+    foreign key("user") references "user"(id)
 );
+
+-- PROD only feature
+-- unique index for tracker table
+-- this index is saying a new row cannot be added unless all 4 values (day, month and year of created_at, and "user") are unique
+/*
+create unique index day_uq on tracker (
+    extract(day from created_at),
+    extract(month from created_at),
+    extract(year from created_at),
+    "user"
+);
+*/
+
+-- statement for elevating user role to admin:
+/*
+update "user" set role=2 where id=1;
+*/
+
+-- statement for adding a row to tracker:
+/*
+insert into tracker (
+overview,
+number_creative_hours,
+rating,
+"user"
+) values (
+'this is an overview. these are the things I did today. how nice',
+2.5,
+2,
+1
+);
+*/
