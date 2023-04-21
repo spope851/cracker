cce7b00a9936896eafe08721e6090a57feadea04
spope <spope851@gmail.com>
2023-04-21 12:58:46 -0600

    remove all logic from ssr

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,45 +4,16 @@ import { useSession } from "next-auth/react"
 import { Box } from "@mui/material"
 import Dashboard from "@/components/dashboard"
 import { Unauthenticated } from "@/components/forms"
-import { RunningAverage } from "@/types"
 import { DashboardFilterContextProvider } from "@/components/dashboard/context"
 
 export async function getServerSideProps() {
-  // const session = await getServerSession(req, res, authOptions)
-  let dashboardFilters: Record<string, string | null> = {
-    runningAvg: null,
-    analyzeEntities: null,
-    tokenTags: null,
-    minTokenCount: null,
-    minEntityCount: null,
-    sentenceTerms: null,
-    hiddenTokens: null,
-    hiddenEntities: null,
-  }
-
-  // const hello = await redis.get("hello")
-  console.log("hello")
-
-  // if (session)
-  // dashboardFilters = await redis.hgetall(`dashboardFilters/${session.user.id}`)
-  return { props: { dashboardFilters } }
+  return { props: { hello: "hello from ssr" } }
 }
 
-export default function Home({
-  dashboardFilters,
-}: {
-  dashboardFilters: {
-    runningAvg: RunningAverage | null
-    analyzeEntities: string | null
-    tokenTags: string | null
-    minTokenCount: string | null
-    minEntityCount: string | null
-    sentenceTerms: string | null
-    hiddenTokens: string | null
-    hiddenEntities: string | null
-  }
-}) {
+export default function Home({ hello }: { hello: string }) {
   const session = useSession()
+
+  console.log(hello)
 
   return (
     <>
@@ -57,7 +28,18 @@ export default function Home({
         m="auto"
       >
         {session.status === "authenticated" ? (
-          <DashboardFilterContextProvider {...dashboardFilters}>
+          <DashboardFilterContextProvider
+            {...{
+              runningAvg: null,
+              analyzeEntities: null,
+              tokenTags: null,
+              minTokenCount: null,
+              minEntityCount: null,
+              sentenceTerms: null,
+              hiddenTokens: null,
+              hiddenEntities: null,
+            }}
+          >
             <Dashboard />
           </DashboardFilterContextProvider>
         ) : (
