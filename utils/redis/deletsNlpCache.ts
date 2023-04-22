5a3652901abbd83b855fd03f2170ab25393dca60
spope <spope851@gmail.com>
2023-04-21 20:40:35 -0600

    remove metrics data from redis in delete cache function as well

--- utils/redis/deletsNlpCache.ts
+++ utils/redis/deletsNlpCache.ts
@@ -4,5 +4,10 @@ const nlpCaches = ["30", "60", "90", "365"]
 
 export const deleteNlpCache = async (user: string) =>
   Promise.all(
-    nlpCaches.map(async (cache) => await redis.del(`nlp/${user}/${cache}`))
+    nlpCaches.map(
+      async (cache) =>
+        await redis
+          .del(`nlp/${user}/${cache}`)
+          .then(async () => await redis.del(`metrics/${user}/${cache}`))
+    )
   )
