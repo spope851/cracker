5c05034ca910e64c4df260b1d2704613f9a7f63a
spope <spope851@gmail.com>
2023-05-06 09:59:45 -0600

    use same client when executing queries in loop

--- graphql/resolvers/uploadTracker.ts
+++ graphql/resolvers/uploadTracker.ts
@@ -17,17 +17,19 @@ class UploadTrackerResolver {
     const {
       user: { id: user },
     } = await getServerSession(req, res, authOptions)
-    const insertRows: Promise<UploadTrackerResponse> = Promise.all(
-      data.map(async (item: TrackerInput, idx: number) => {
-        const { overview, numberCreativeHours, rating } = item
-        const today = new Date()
-        today.setDate(today.getDate() - (idx + 1))
-        const createdAt = `${today.getFullYear()}-${
-          today.getMonth() + 1
-        }-${today.getDate()}`
-        return await pool
-          .query(
-            `INSERT INTO tracker (
+
+    return await pool.connect().then((client: typeof pool) => {
+      const insertRows: Promise<UploadTrackerResponse> = Promise.all(
+        data.map(async (item: TrackerInput, idx: number) => {
+          const { overview, numberCreativeHours, rating } = item
+          const today = new Date()
+          today.setDate(today.getDate() - (idx + 1))
+          const createdAt = `${today.getFullYear()}-${
+            today.getMonth() + 1
+          }-${today.getDate()}`
+          return await client
+            .query(
+              `INSERT INTO tracker (
                 overview, 
                 number_creative_hours, 
                 rating, 
@@ -41,47 +43,47 @@ class UploadTrackerResolver {
                 TO_TIMESTAMP($4, 'YYYY-MM-DD'),
                 $5
               );`,
-            [overview, numberCreativeHours, rating, createdAt, user]
-          )
-          .catch((e: PgQueryError) => {
-            if (e.code === "23503") {
-              const details = postgresErrorDetails(e.detail)
-              // code:
-              // detail: 'Key (user)=(1) is not present in table "user".'
-              return {
-                errors: [
-                  {
-                    field: details[1],
-                    message: `id ${details[3] + details[4]}`,
-                  },
-                ],
+              [overview, numberCreativeHours, rating, createdAt, user]
+            )
+            .catch((e: PgQueryError) => {
+              if (e.code === "23503") {
+                const details = postgresErrorDetails(e.detail)
+                // code:
+                // detail: 'Key (user)=(1) is not present in table "user".'
+                return {
+                  errors: [
+                    {
+                      field: details[1],
+                      message: `id ${details[3] + details[4]}`,
+                    },
+                  ],
+                }
               }
-            }
-            console.log(e)
-            return {
-              errors: [{ field: "unknown", massage: "unhandled error" }],
-            }
-          })
-      })
-    )
-      .then(() => {
-        return {
-          uploaded: `successfully uploaded ${data.length} days worth of data`,
-        }
-      })
-      .catch((e) => {
-        console.log(e)
-        return {
-          errors: [
-            {
-              field: "unknown",
-              message: `something went wrong. check the database logs`,
-            },
-          ],
-        }
-      })
-
-    return insertRows
+              console.log(e)
+              return {
+                errors: [{ field: "unknown", massage: "unhandled error" }],
+              }
+            })
+        })
+      )
+        .then(() => {
+          return {
+            uploaded: `successfully uploaded ${data.length} days worth of data`,
+          }
+        })
+        .catch((e) => {
+          console.log(e)
+          return {
+            errors: [
+              {
+                field: "unknown",
+                message: `something went wrong. check the database logs`,
+              },
+            ],
+          }
+        })
+      return insertRows
+    })
   }
 }
 
