ee9d039a9db25b35e2909a071dddc79f4aac94b5
spope <spope851@gmail.com>
2023-05-14 16:27:41 -0600

    remove unused fields from schema

--- graphql/client/global/featureFlagsQuery.ts
+++ graphql/client/global/featureFlagsQuery.ts
@@ -2,9 +2,7 @@ import { graphql } from "@/generated"
 export const FEATURE_FLAGS_QUERY = graphql(`
   query FeatureFlags {
     featureFlags {
-      id
       name
-      description
       isEnabled
       requiredRole
     }
