d403c2b4e64aa8dca1f49f3b78ec64223a6834ac
spencer pope <34104632+spope851@users.noreply.github.com>
2023-05-15 17:05:57 -0600

    use same client when executing queries in loop (#24)

--- utils/postgres.ts
+++ utils/postgres.ts
@@ -3,7 +3,7 @@ import pkg from "pg"
 delete pkg.native
 const { Pool } = pkg
 
-export const pool = new Pool({
+export const pool: typeof Pool = new Pool({
   user: keys.pgUser,
   host: keys.serverHost,
   database: keys.pgDatabase,
