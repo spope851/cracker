24dc3cc47845259a7c6ebf96f499c07db648a139
spope <spope851@gmail.com>
2023-05-25 18:29:12 -0600

    refactor init script order

--- server/database/sql/create_triggers.sql
+++ server/database/sql/create_triggers.sql
@@ -13,29 +13,29 @@ execute procedure trigger_set_timestamp();
 CREATE TRIGGER insert_words_from_overview AFTER INSERT 
 ON tracker
 FOR EACH ROW
-execute procedure trigger_insert_words()
+execute procedure trigger_insert_words();
 
 CREATE TRIGGER delete_words_from_overview AFTER UPDATE 
 ON tracker
 FOR EACH ROW
-execute procedure trigger_delete_words()
+execute procedure trigger_delete_words();
 
 CREATE TRIGGER update_words_from_overview AFTER UPDATE 
 ON tracker
 FOR EACH ROW
-execute procedure trigger_insert_words()
+execute procedure trigger_insert_words();
 
 CREATE TRIGGER insert_sentences_from_overview AFTER INSERT 
 ON tracker
 FOR EACH ROW
-execute procedure trigger_insert_sentences()
+execute procedure trigger_insert_sentences();
 
 CREATE TRIGGER delete_sentences_from_overview AFTER UPDATE 
 ON tracker
 FOR EACH ROW
-execute procedure trigger_delete_sentences()
+execute procedure trigger_delete_sentences();
 
 CREATE TRIGGER update_sentences_from_overview AFTER UPDATE 
 ON tracker
 FOR EACH ROW
-execute procedure trigger_insert_sentences()
+execute procedure trigger_insert_sentences();
