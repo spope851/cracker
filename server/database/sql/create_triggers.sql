64e01c62ff1840b5faa4dcc6f210665d48ade08d
spencer pope <34104632+spope851@users.noreply.github.com>
2023-05-27 13:05:45 -0600

    Refactor dashboard basic db (#32)
    * triggers to insert words and sentences when user data is tracked. views for dashboard words and sentences. functions for getting dashboard words, sentences, counts based on pre query filters. update resolver to return data from functions instead of cursors from stored procedure
    
    * refactor init script order

--- server/database/sql/create_triggers.sql
+++ server/database/sql/create_triggers.sql
@@ -9,3 +9,33 @@ before
 update on tracker
 for each row
 execute procedure trigger_set_timestamp();
+
+CREATE TRIGGER insert_words_from_overview AFTER INSERT 
+ON tracker
+FOR EACH ROW
+execute procedure trigger_insert_words();
+
+CREATE TRIGGER delete_words_from_overview AFTER UPDATE 
+ON tracker
+FOR EACH ROW
+execute procedure trigger_delete_words();
+
+CREATE TRIGGER update_words_from_overview AFTER UPDATE 
+ON tracker
+FOR EACH ROW
+execute procedure trigger_insert_words();
+
+CREATE TRIGGER insert_sentences_from_overview AFTER INSERT 
+ON tracker
+FOR EACH ROW
+execute procedure trigger_insert_sentences();
+
+CREATE TRIGGER delete_sentences_from_overview AFTER UPDATE 
+ON tracker
+FOR EACH ROW
+execute procedure trigger_delete_sentences();
+
+CREATE TRIGGER update_sentences_from_overview AFTER UPDATE 
+ON tracker
+FOR EACH ROW
+execute procedure trigger_insert_sentences();
