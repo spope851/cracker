519fa6b18c296facbe92046a2e41a189984f1fc7
spope <spope851@gmail.com>
2023-06-10 23:42:32 -0600

    adjust delete cache function for new dashboard cache structure

--- utils/redis/deleteNlpCache.ts
+++ utils/redis/deleteNlpCache.ts
@@ -4,12 +4,13 @@ import redis from "."
 const nlpCaches = ["30", "60", "90", "365"]
 
 export const deleteNlpCache = async (user: string) =>
-  Promise.all(
-    nlpCaches.map(
+  Promise.all([
+    await redis.del(`${CACHE_KEYS.basicDashboardWords}/${user}`),
+    await redis.del(`${CACHE_KEYS.basicDashboardSentences}/${user}`),
+    ...nlpCaches.map(
       async (cache) =>
         await redis
           .del(`${CACHE_KEYS.premiumDashboard}/${user}/${cache}`)
-          .then(async () => await redis.del(`basic/${user}/${cache}`))
           .then(async () => await redis.del(`metrics/${user}/${cache}`))
-    )
-  )
+    ),
+  ])
